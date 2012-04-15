bec6bd3501ae4abca108cc9a3a01da2dd237cdf9
Andrew Smillie <ajsmillie@gmail.com>
2012-04-15 19:15:41 +0100

    enabling NFS mount management for picon

--- manifests/nodes/picon.geeksoc.org.pp
+++ manifests/nodes/picon.geeksoc.org.pp
@@ -2,11 +2,11 @@
 node 'picon.geeksoc.org' {
 	$sshd_config_PermitRootLogin = 'no'
 	
-    /* nfs::share { "home":
-        path:    => "/home",
-        allowed: => "130.159.141.64/26",
-        options: => "rw,sync,no_subtree_check",
-    }*/
+    nfs::share { "home":
+        path    => "/home",
+        allowed => "130.159.141.64/26",
+        options => "rw,sync,no_subtree_check",
+    }
 
 	include global
 }
