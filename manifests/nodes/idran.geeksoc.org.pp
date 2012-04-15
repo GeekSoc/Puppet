af2efcd6a29a0504f7e73a8685998ec5db43af57
Andrew Smillie <ajsmillie@gmail.com>
2012-04-15 22:10:06 +0100

    Added idran.geeksoc.org node file

---
+++ manifests/nodes/idran.geeksoc.org.pp
@@ -0,0 +1,13 @@
+
+node 'idran.geeksoc.org' {
+
+    mount { "/home":
+        device  => "storage.geeksoc.org:/home",
+        fstype  => "nfs",
+        ensure  => "mounted",
+        options => "rw,hard,intr",
+        atboot  => true,
+    }
+
+	include global
+}
