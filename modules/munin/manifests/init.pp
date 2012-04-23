0d00d3b71167508e4ae7b062e3f9f22298a2a7a6
Andrew Smillie <ajsmillie@gmail.com>
2012-04-23 20:53:42 +0100

    Munin config file issues

--- modules/munin/manifests/init.pp
+++ modules/munin/manifests/init.pp
@@ -3,9 +3,12 @@ class munin::node {
 	package { "munin-node": ensure => installed }
 	service { "munin-node":	ensure => running, enable => true }
 	
-	file { "/etc/munin-node.conf":
+	file { "/etc/munin/munin-node.conf":
 		source => 'puppet:///modules/munin/munin-node.conf',
-		ensure => file,
+		ensure => present,
 		notify => Service["munin-node"]
+	}
+	file { "/etc/munin-node.conf":
+		ensure => purged,
 	}
 }
