133da7ecf67b7065676a7aed880acf6e04bdde28
Andrew Smillie <ajsmillie@gmail.com>
2012-04-24 21:16:57 +0100

    fail2ban jail.conf

--- modules/fail2ban/manifests/init.pp
+++ modules/fail2ban/manifests/init.pp
@@ -10,5 +10,12 @@ class fail2ban {
     file { "/etc/fail2ban/fail2ban.conf":
         ensure => present,
         source => "puppet:///modules/fail2ban/fail2ban.conf",
+		notify => Service["fail2ban"]
+    }
+
+    file { "/etc/fail2ban/jail.conf":
+        ensure => present,
+        source => "puppet:///modules/fail2ban/jail.conf",
+		notify => Service["fail2ban"]
     }
 }
