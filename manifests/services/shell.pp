92436ae15561b6bb0b03e7305a5d2cb72466982c
Andrew Smillie <ajsmillie@gmail.com>
2012-05-25 19:52:45 +0100

    More dev related packages (for Shell)

--- manifests/services/shell.pp
+++ manifests/services/shell.pp
@@ -1,24 +1,40 @@
 class shell {
 	$sshd_config_AllowGroups = 'members gssg gsag root'
+
 	include gs-scripts::listusers
 	
 	package { [ "alpine",
+				"autoconf",
+				"automake",
+				"binutils",
+				"bison",
 				"bzr",
 				"cvs",
 				"darcs",
 				"elinks",
 				"emacs",
 				"erlang",
+				"flex",
 				"fuse-sshfs",
+				"gcc",
+				"gcc-c++",
+				"gettext",
 				"haskell-platform",
 				"irssi",
+				"java-1.6.0-openjdk",
 				"jwhois",
+				"libtool",
 				"lynx",
+				"make",
 				"mc",
+				"patch",
 				"perl",
 				"php",
+				"pkgconfig",
 				"python",
 				"python-ldap",
+				"redhat-rpm-config",
+				"rpm-build",
 				"ruby",
 				"rubygems",
 				"samba-client",
@@ -26,7 +42,7 @@ class shell {
 				"subversion",
 				"telnet",
 				"texlive-latex",
-				"zsh",]:
+				"zsh", ]:
         ensure => installed,
     }
 	
