faa586b3c0102e7699210ef81c8a486bb602eada
Andrew Smillie <ajsmillie@gmail.com>
2012-09-11 22:06:42 +0100

    Base template for hackathon dev boxes

---
+++ manifests/roles/hackathon.pp
@@ -0,0 +1,56 @@
+class hackathon {
+	
+	# Message of the day
+	file { '/etc/motd':
+        content => "
+ _   _   ___      _      ___ _   _     _____       
+| | | | /   |    | |    /   | | | |   |  _  |      
+| |_| |/ /| | ___| | __/ /| | |_| |__ | |/' |_ __  
+|  _  / /_| |/ __| |/ / /_| | __| '_ \|  /| | '_ \ 
+| | | \___  | (__|   <\___  | |_| | | \ |_/ / | | |
+\_| |_/   |_/\___|_|\_\   |_/\__|_| |_|\___/|_| |_|                                        
+
+Primary Contact: GSAG - support@geeksoc.org 
+Purpose: Hackathon Dev Box
+
+This server is Puppet managed - local changes may be overwritten!
+
+" 
+    }
+	
+	$sshd_config_AllowGroups = 'members gssg gsag root'
+	
+	package { [ "autoconf",
+				"automake",
+				"binutils",
+				"bison",
+				"bzr",
+				"cvs",
+				"darcs",
+				"elinks",
+				"emacs",
+				"erlang",
+				"flex",
+				"sshfs",
+				"gcc",
+				"haskell-platform",
+				"openjdk-6-jdk",
+				"openjdk-6-jre",
+				"lynx",
+				"make",
+				"mc",
+				"perl",
+				"php5",
+				"php5-ldap",
+				"ruby",
+				"rubygems",
+				"smbclient",
+				"siege",
+				"subversion",
+				"telnet",
+				"texlive-latex3", ]:
+        ensure => installed,
+    }
+	
+    include global
+}
