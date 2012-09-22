88b46b89c6ad9c981fa87a2748c3cfda4e94b6cc
Andrew Smillie <ajsmillie@gmail.com>
2012-09-22 23:28:58 +0100

    Adding IRC MOTD provided by MrRandom/amcghie

--- modules/ircd/manifests/init.pp
+++ modules/ircd/manifests/init.pp
@@ -7,8 +7,18 @@ class ircd {
         source  => "puppet:///modules/ircd/ircd.conf",
     }
 
+	file { "/home/ircd/ircd/ircd.motd":
+        owner   => "ircd",
+        group   => "ircd",
+        mode    => 0600,
+        source  => "puppet:///modules/ircd/ircd.motd",
+    }
+
 	exec { "/home/ircd/ircd/unreal rehash":
-		subscribe => File["/home/ircd/ircd/unrealircd.conf"],
+		subscribe => [
+			File["/home/ircd/ircd/unrealircd.conf"],
+			File["/home/ircd/ircd/ircd.motd"]
+			],
 		refreshonly => true
 	}
 
