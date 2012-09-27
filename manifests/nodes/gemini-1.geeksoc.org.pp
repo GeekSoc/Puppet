6345ff8e51d69902683d7a9e83224fd80c8d5a3b
Andrew Smillie <ajsmillie@gmail.com>
2012-09-27 19:47:52 +0100

    Gemini-1 and Hack-6 monitoring

--- manifests/nodes/gemini-1.geeksoc.org.pp
+++ manifests/nodes/gemini-1.geeksoc.org.pp
@@ -4,16 +4,15 @@ node 'gemini-1.geeksoc.org' {
 	# Message of the day
 	file { '/etc/motd':
         content => "
-                   
  _____                _       _        __  
 |  __ \              (_)     (_)      /  | 
 | |  \/ ___ _ __ ___  _ _ __  _ ______`| | 
 | | __ / _ \ '_ ` _ \| | '_ \| |______|| | 
 | |_\ \  __/ | | | | | | | | | |      _| |_
  \____/\___|_| |_| |_|_|_| |_|_|      \___/
-                                               
+
 Primary Contact: GSAG - gsag@geeksoc.org 
-Purpose: Xen dom0 for Development
+Purpose: Xen dom0 for Development                                                                   
 
 This server is Puppet managed - local changes may be overwritten!
 
