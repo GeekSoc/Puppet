442a0189417ef5c541b070d0ba056c05fee9f3f3
Andrew Smillie <ajsmillie@gmail.com>
2012-09-27 19:08:11 +0100

    Gemini-1 node file

---
+++ manifests/nodes/gemini-1.geeksoc.org.pp
@@ -0,0 +1,26 @@
+
+node 'gemini-1.geeksoc.org' {
+
+	# Message of the day
+	file { '/etc/motd':
+        content => "
+                   
+ _____                _       _        __  
+|  __ \              (_)     (_)      /  | 
+| |  \/ ___ _ __ ___  _ _ __  _ ______`| | 
+| | __ / _ \ '_ ` _ \| | '_ \| |______|| | 
+| |_\ \  __/ | | | | | | | | | |      _| |_
+ \____/\___|_| |_| |_|_|_| |_|_|      \___/
+                                               
+Primary Contact: GSAG - gsag@geeksoc.org 
+Purpose: Xen dom0 for Development
+
+This server is Puppet managed - local changes may be overwritten!
+
+" 
+    }
+
+
+	# Roles
+	include global
+}
