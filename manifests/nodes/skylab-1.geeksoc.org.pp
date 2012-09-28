ca14addd1d693c9505f991231d9d99e8f7a8f5d5
wnelson <wnelson@geeksoc.org>
2012-09-28 15:15:05 +0100

    added nodes for skylab 1 and 2

---
+++ manifests/nodes/skylab-1.geeksoc.org.pp
@@ -0,0 +1,27 @@
+
+node 'skylab-1.geeksoc.org' {
+
+	# Message of the day
+	file { '/etc/motd':
+        content => "
+     _          _       _           __  
+    | |        | |     | |         /  | 
+ ___| | ___   _| | __ _| |__ ______`| | 
+/ __| |/ / | | | |/ _` | '_ \______|| | 
+\__ \   <| |_| | | (_| | |_) |     _| |_
+|___/_|\_\\__, |_|\__,_|_.__/      \___/
+           __/ |                        
+          |___/                         
+
+Primary Contact: GSAG - gsag@geeksoc.org 
+Purpose: Open Development Box                                                                   
+
+This server is Puppet managed - local changes may be overwritten!
+
+" 
+    }
+
+
+	# Roles
+	include global
+}
---
+++ manifests/nodes/skylab-2.geeksoc.org.pp
@@ -0,0 +1,27 @@
+
+node 'skylab-2.geeksoc.org' {
+
+	# Message of the day
+	file { '/etc/motd':
+        content => "
+     _          _       _           _____ 
+    | |        | |     | |         / __  \
+ ___| | ___   _| | __ _| |__ ______`' / /'
+/ __| |/ / | | | |/ _` | '_ \______| / /  
+\__ \   <| |_| | | (_| | |_) |     ./ /___
+|___/_|\_\\__, |_|\__,_|_.__/      \_____/
+           __/ |                          
+          |___/                           
+
+Primary Contact: GSAG - gsag@geeksoc.org 
+Purpose: Open Development Box                                                                   
+
+This server is Puppet managed - local changes may be overwritten!
+
+" 
+    }
+
+
+	# Roles
+	include global
+}
