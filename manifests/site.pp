5b2dab03aca60f9ff036e4a3fa6fadd99de329ce
Andrew Smillie <ajsmillie@gmail.com>
2012-10-09 19:33:11 +0100

    extlookup source

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,9 @@
 import "roles/*.pp"
 import "nodes/*.pp"
 
+$extlookup_datadir = "/etc/puppet-private"
+$extlookup_precedence = ["%{fqdn}", "domain_%{domain}", "common"]
+
 # The filebucket option allows for file backups to the server
 filebucket { main: server => 'puppet' }
 
