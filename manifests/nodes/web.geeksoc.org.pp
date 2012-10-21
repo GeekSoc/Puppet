c1e88842f656f414d9d322cb1fdc5aff0926edef
Andrew Smillie <ajsmillie@gmail.com>
2012-10-21 19:38:39 +0100

    Basic manifest for web server VM

---
+++ manifests/nodes/web.geeksoc.org.pp
@@ -0,0 +1,27 @@
+
+node 'web.geeksoc.org' {
+
+	# Modules
+	
+	# Message of the day
+	file { '/etc/motd':
+        content => " 
+ _    _      _           _____           _     _____            
+| |  | |    | |         |  __ \         | |   /  ___|           
+| |  | | ___| |__       | |  \/ ___  ___| | __\ `--.  ___   ___ 
+| |/\| |/ _ \ '_ \      | | __ / _ \/ _ \ |/ / `--. \/ _ \ / __|
+\  /\  /  __/ |_) |  _  | |_\ \  __/  __/   < /\__/ / (_) | (__ 
+ \/  \/ \___|_.__/  (_)  \____/\___|\___|_|\_\\\____/ \___/ \___|
+
+Primary Contact: GSAG - gsag@geeksoc.org
+Purpose: Primary Web Server
+
+This server is Puppet managed - local changes may be overwritten!
+
+" 
+    }
+	
+	
+	# Roles
+	# include web
+}
