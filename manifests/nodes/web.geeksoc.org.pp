42ecc0aea184cc11666a4ed980ef109a147e94e5
Andrew Smillie <ajsmillie@gmail.com>
2012-10-21 20:26:53 +0100

    Web VM role updates

--- manifests/nodes/web.geeksoc.org.pp
+++ manifests/nodes/web.geeksoc.org.pp
@@ -21,7 +21,44 @@ This server is Puppet managed - local changes may be overwritten!
 " 
     }
 	
+	mount { "/home":
+        device  => "storage.geeksoc.org:/home",
+        fstype  => "nfs",
+        ensure  => "mounted",
+        options => "rw,hard,intr",
+        atboot  => true,
+    }
+
+	include apache
+	# include php
+	# include mysql
+	# include mysql::phpMyAdmin
+	# include etherpad-lite
 	
-	# Roles
-	include web
+	apache::website { "geeksoc.org":
+        server_aliases => [ "www.geeksoc.org" ],
+		# https => true,
+    }
+	apache::website { "bugs.geeksoc.org":
+        server_aliases => [ "www.bugs.geeksoc.org" ],
+		# https => true,
+    }
+	apache::website { "dev.geeksoc.org":
+        server_aliases => [ "www.dev.geeksoc.org" ],
+		# https => true,
+    }
+	apache::website { "wiki.geeksoc.org":
+        server_aliases => [ "www.wiki.geeksoc.org" ],
+		# https => true,
+    }
+	apache::website { "accounts.geeksoc.org":
+        server_aliases => [ "www.accounts.geeksoc.org" ],
+		# https => true,
+    }
+	apache::website { "stats.irc.geeksoc.org":
+        server_aliases => [ "www.irc.stats.geeksoc.org" ],
+		# https => true,
+    }
+
+    include global
 }
