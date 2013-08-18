ce9db1f1a24d4e259fb39ff87fb997139d1bd047
Andrew Smillie <ajsmillie@gmail.com>
2013-08-18 14:56:36 +0100

    New VM for mail server test

---
+++ manifests/nodes/langara.geeksoc.org.pp
@@ -0,0 +1,28 @@
+
+node 'langara.geeksoc.org' {
+
+	# Modules
+
+	# Message of the day
+	file { '/etc/motd':
+        content => "
+ _                                        _____           _                   
+| |                                      |  __ \         | |                  
+| |     __ _ _ __   __ _  __ _ _ __ __ _ | |  \/ ___  ___| | _____  ___   ___ 
+| |    / _` | '_ \ / _` |/ _` | '__/ _` || | __ / _ \/ _ \ |/ / __|/ _ \ / __|
+| |___| (_| | | | | (_| | (_| | | | (_| || |_\ \  __/  __/   <\__ \ (_) | (__ 
+\_____/\__,_|_| |_|\__, |\__,_|_|  \__,_(_)____/\___|\___|_|\_\\\____/ \___/ \___|
+                   __/ |                                                     
+                  |___/                                                      
+
+Primary Contact: GSAG - gsag@geeksoc.org
+Purpose: Mail server
+
+This server is Puppet managed - local changes may be overwritten!
+
+" 
+    }
+
+	# Roles
+	include global
+}
