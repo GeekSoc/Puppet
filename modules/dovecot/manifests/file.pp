06a75d132b6e130e43eab262bf63f89cbac3ccc1
Stuart Swindells <sswindells@geeksoc.org>
2013-08-28 11:12:43 +0100

    Add Dovecot module, properly this time

---
+++ modules/dovecot/manifests/file.pp
@@ -0,0 +1,28 @@
+# Define: dovecot::file
+#
+# Manage dovecot configuration files under /etc/dovecot/.
+#
+# Example Usage:
+#     dovecot::file { 'dovecot-sql.conf.ext':
+#         source => 'puppet:///modules/mymodule/dovecot-sql.conf.ext',
+#     }
+#
+define dovecot::file (
+    $owner   = 'root',
+    $group   = 'root',
+    $mode    = '0644',
+    $content = undef,
+    $source  = undef
+) {
+    file { "/etc/dovecot/${title}":
+        owner   => $owner,
+        group   => $group,
+        mode    => $mode,
+        content => $content,
+        source  => $source,
+        require => Package[$dovecot::packages],
+        notify  => Service['dovecot'],
+        replace => true,
+    }
+}
+
