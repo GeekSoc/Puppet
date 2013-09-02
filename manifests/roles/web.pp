0343596aed27f664c37cfb6f9212b0b0dfef63a7
Andrew Smillie <ajsmillie@gmail.com>
2013-09-02 19:05:16 +0100

    Setup vhost + DNS for webmail

--- manifests/roles/web.pp
+++ manifests/roles/web.pp
@@ -15,33 +15,37 @@ class web {
 	include etherpad-lite
 	
 	apache::website { "geeksoc.org":
-        server_aliases => [ "www.geeksoc.org" ],
+    server_aliases => [ "www.geeksoc.org" ],
 		https => true,
-    }
+  }
 	apache::website { "bugs.geeksoc.org":
-        server_aliases => [ "www.bugs.geeksoc.org" ],
+    server_aliases => [ "www.bugs.geeksoc.org" ],
 		https => true,
-    }
+  }
 	apache::website { "dev.geeksoc.org":
-        server_aliases => [ "www.dev.geeksoc.org" ],
+   server_aliases => [ "www.dev.geeksoc.org" ],
 		https => true,
-    }
+  }
 	apache::website { "wiki.geeksoc.org":
-        server_aliases => [ "www.wiki.geeksoc.org" ],
+    server_aliases => [ "www.wiki.geeksoc.org" ],
 		https => true,
-    }
+  }
 	apache::website { "accounts.geeksoc.org":
-        server_aliases => [ "www.accounts.geeksoc.org" ],
+  server_aliases => [ "www.accounts.geeksoc.org" ],
 		https => true,
-    }
+  }
   apache::website {"api.accounts.geeksoc.org":
-      server_aliases => [ "www.api.accounts.geeksoc.org" ],
-      https          => true,
+    server_aliases => [ "www.api.accounts.geeksoc.org" ],
+    https          => true,
   }
 	apache::website { "stats.irc.geeksoc.org":
-        server_aliases => [ "www.irc.stats.geeksoc.org" ],
+    server_aliases => [ "www.irc.stats.geeksoc.org" ],
 		https => true,
-    }
+  }
+	apache::website { "webmail.geeksoc.org":
+    server_aliases => [ "www.webmail.geeksoc.org" ],
+		https => true,
+  }
 
     include global
 }
