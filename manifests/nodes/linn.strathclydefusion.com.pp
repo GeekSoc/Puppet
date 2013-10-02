2114f7d4a2af2d2d67a91e19b09e7133ce449528
Andrew Smillie <ajsmillie@gmail.com>
2013-10-02 21:12:11 +0100

    use correct syntax to specify sudoers

--- manifests/nodes/linn.strathclydefusion.com.pp
+++ manifests/nodes/linn.strathclydefusion.com.pp
@@ -1,6 +1,12 @@
 node 'linn.strathclydefusion.com' {
 
-    $sudoers = ["%soc-fusion"]
+    class {'ssh:sshd':
+        sshd_config_AllowGroups => 'soc-fusion sysadmin gsag root',
+    }
+
+    class {'sudo':
+        sudoers => '%soc-fusion',
+    }
 
     #Roles
     include global
