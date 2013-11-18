07d08f63c024bba7ce66a43101b9313036098a1b
Andrew Smillie <ajsmillie@gmail.com>
2013-11-18 19:45:59 +0000

    Basic node file for magrathea

---
+++ manifests/nodes/magrathea.geeksoc.org.pp
@@ -0,0 +1,23 @@
+
+node 'magrathea.geeksoc.org' {
+
+	# Message of the day
+	file { '/etc/motd':
+        content => " 
+___  ___                      _   _                
+|  \/  |                     | | | |               
+| .  . | __ _  __ _ _ __ __ _| |_| |__   ___  __ _ 
+| |\/| |/ _` |/ _` | '__/ _` | __| '_ \ / _ \/ _` |
+| |  | | (_| | (_| | | | (_| | |_| | | |  __/ (_| |
+\_|  |_/\__,_|\__, |_|  \__,_|\__|_| |_|\___|\__,_|
+               __/ |                               
+              |___/                                
+
+Primary Contact: GSAG - support@geeksoc.org
+Purpose: KVM Host
+
+" 
+    }
+
+    include global
+}
