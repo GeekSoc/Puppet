31447a104b2f73a0b29dc489e78083eb200c7004
Robert Wilson <robert.wilson1717@gmail.com>
2014-02-06 20:31:46 +0000

    Added basic RSpec for Eridani-2

---
+++ spec/hosts/eridani-2_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'eridani-2.geeksoc.org' do
+  let(:node) { 'eridani-2.geeksoc.org' }
+  let(:facts) {:operatingsystem => 'Solaris' }
+  
+  it {should compile}
+  
+  it { should_not contain_mount('/home') }
+  it { should contain_class('sol_global') }
+  
+end
