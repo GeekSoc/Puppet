98cae732aa9584d2300ad3f57d1f0df4e338c01d
Rob Wilson <rwilson@geeksoc.org>
2015-01-22 01:36:38 +0000

    readd https stuff for apache

--- modules/custom/manifests/apache/website.pp
+++ modules/custom/manifests/apache/website.pp
@@ -6,12 +6,27 @@ define custom::apache::website (
 
     apache::vhost { $name:
         serveraliases => $server_aliases,
-        ssl           => $https,
+        ssl           => false,
         serveradmin   => $server_admin,
         docroot       => "/var/www/vhosts/${name}/public_html",
         options       => ["+Indexes"],
         override      => ["All"],
         port          => 80,
+    }
+
+    if $https {
+
+        apache::vhost { "${name}-ssl":
+        servername    => $name,
+        serveraliases => $server_aliases,
+        ssl           => true,
+        serveradmin   => $server_admin,
+        docroot       => "/var/www/vhosts/${name}/public_html",
+        options       => ["+Indexes"],
+        override      => ["All"],
+        port          => 433,
+    }
+
     }
 
 }
