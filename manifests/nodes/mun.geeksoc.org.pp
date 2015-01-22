28fb90c27b77a41a9b90e36eb196d91a1766d8d7
Rob Wilson <rwilson@geeksoc.org>
2015-01-22 20:31:53 +0000

    Added mun to do nagios on

---
+++ manifests/nodes/mun.geeksoc.org.pp
@@ -0,0 +1,50 @@
+
+node 'mun.geeksoc.org' {
+  
+  $nagiosusers ='asmillie,kcoyle,pkinnaird,wnelson,amckinlay,fvalente,\
+sswindells,fmacisaac,rwilson'
+
+  include apache
+
+  class { 'nagios::server':
+  apache_httpd                                 => false,
+  apache_httpd_ssl                             => false,
+  apache_httpd_conf_content                    => '',
+  apache_httpd_conf_source                     =>
+'puppet:///modules/custom/nagios-apache.conf',
+  apache_httpd_htpasswd_source                 => false,
+  php                                          => false,
+  php_apc                                      => false,
+  cgi_authorized_for_system_information        => $nagiosusers,
+  cgi_authorized_for_configuration_information => $nagiosusers,
+  cgi_authorized_for_system_commands           => $nagiosusers,
+  cgi_authorized_for_all_services              => '*',
+  cgi_authorized_for_all_hosts                 => '*',
+  cgi_authorized_for_all_service_commands      => $nagiosusers,
+  cgi_authorized_for_all_host_commands         => $nagiosusers,
+  cgi_default_statusmap_layout                 => '5',
+  admin_email                                  => 'nagios-alert@geeksoc.org',
+  
+}
+  
+
+
+  # Message of the day
+  file { '/etc/motd':
+        content => "
+min.geeksoc.org Sorry no ascii     
+
+                                                          
+
+Primary Contact: GSAG - gsag@geeksoc.org 
+Purpose: Backup and Monitoring server
+
+This server is Puppet managed - local changes may be overwritten!
+
+"
+    }
+
+
+  # Roles
+  include global
+}
