eb465bae016a64ade05dd2be05b5ed5357aec0f6
Rob Wilson <rwilson@geeksoc.org>
2015-02-12 20:11:32 +0000

    Stop tauron from nagiosing

--- manifests/nodes/tauron.geeksoc.org.pp
+++ manifests/nodes/tauron.geeksoc.org.pp
@@ -1,31 +1,8 @@
 
 node 'tauron.geeksoc.org' {
-  
-  $nagiosusers ='asmillie,kcoyle,pkinnaird,wnelson,amckinlay,fvalente,\
-sswindells,fmacisaac,rwilson'
 
   include apache
 
-  class { 'nagios::server':
-  apache_httpd                                 => false,
-  apache_httpd_ssl                             => false,
-  apache_httpd_conf_content                    => '',
-  apache_httpd_conf_source                     =>
-'puppet:///modules/custom/nagios-apache.conf',
-  apache_httpd_htpasswd_source                 => false,
-  php                                          => false,
-  php_apc                                      => false,
-  cgi_authorized_for_system_information        => $nagiosusers,
-  cgi_authorized_for_configuration_information => $nagiosusers,
-  cgi_authorized_for_system_commands           => $nagiosusers,
-  cgi_authorized_for_all_services              => '*',
-  cgi_authorized_for_all_hosts                 => '*',
-  cgi_authorized_for_all_service_commands      => $nagiosusers,
-  cgi_authorized_for_all_host_commands         => $nagiosusers,
-  cgi_default_statusmap_layout                 => '5',
-  admin_email                                  => 'nagios-alert@geeksoc.org',
-  
-}
   include munin::server
 
     nfs::share { 'home':
